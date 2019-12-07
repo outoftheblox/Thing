150359f4164fa8375b1dc8c2badc652c7b63dd02
Gerben <gerbenvaneerten@gmail.com>
2019-12-07 13:54:13 +0100

    OTA spiffs

---
+++ src/OtaSpiffsThing.cpp
@@ -0,0 +1,39 @@
+#include "OtaSpiffsThing.h"
+#include "SpiffsTextSettings.h"
+
+using namespace ootb;
+
+OtaSpiffsThing::OtaSpiffsThing(String& name)
+:
+filename(name)
+{
+}
+
+OtaSpiffsThing::OtaSpiffsThing(const char* name)
+:
+filename(String(name))
+{
+}
+
+OtaSpiffsThing::~OtaSpiffsThing()
+{
+}
+
+void OtaSpiffsThing::begin()
+{
+    SpiffsTextSettings settings;
+    settings.begin();
+    bool success = settings.load(filename.c_str());
+    if (!success)
+    {
+        Serial.println("Failed to load config");
+    }
+    if (success)
+    {
+        String password = settings.readString();
+        if (password.length() == 0)
+            Serial.println("Failed to load password");
+        setPassword(password.c_str());
+    }
+    OtaThing::begin();
+}
---
+++ src/OtaSpiffsThing.h
@@ -0,0 +1,26 @@
+#ifndef _OtaSpiffsThing_H
+#define _OtaSpiffsThing_H
+
+#include "OtaThing.h"
+
+namespace ootb
+{
+
+  class OtaSpiffsThing : public OtaThing
+  {
+    private:
+      String filename;
+
+    public:
+      OtaSpiffsThing(const char* name);
+      OtaSpiffsThing(String& filename);
+      ~OtaSpiffsThing();
+
+      void begin();
+
+    private:
+
+  };
+}
+
+#endif
