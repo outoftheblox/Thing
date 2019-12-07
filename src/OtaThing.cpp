20a47f7a4ebe417254cd2a95e9c4cd3a963e28bd
Gerben <gerbenvaneerten@gmail.com>
2019-12-07 02:44:29 +0100

    Added ota callbacks

--- src/OtaThing.cpp
+++ src/OtaThing.cpp
@@ -12,26 +12,39 @@ OtaThing::~OtaThing()
 {
 }
 
+void OtaThing::onOtaStart(std::function<void()> f)
+{
+    otaStartCallback = f;
+}
+
+void OtaThing::onOtaEnd(std::function<void()> f)
+{
+    otaEndCallback = f;
+}
+
 void OtaThing::begin()
 {
-  ArduinoOTA.onStart([]() {
-    Serial.println("Start");
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-  ArduinoOTA.begin();
+    ArduinoOTA.setPasswordHash("bf3967146d23842e157da467392c4520");
+    ArduinoOTA.onStart([]() {
+        Serial.println("Start");
+        if (otaStartCallback) otaStartCallback();
+    });
+    ArduinoOTA.onEnd([]() {
+        Serial.println("\nEnd");
+        if (otaStartCallback) otaEndCallback();
+    });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+    ArduinoOTA.onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+        else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+    ArduinoOTA.begin();
 }
 
 void OtaThing::handle()
--- src/OtaThing.h
+++ src/OtaThing.h
@@ -4,15 +4,22 @@
 namespace ootb
 {
 
-  class OtaThing
-  {
+class OtaThing
+{
     public:
-      OtaThing();
-      ~OtaThing();
+    OtaThing();
+    ~OtaThing();
 
-      void begin();
-      void handle();
-  };
+    void begin();
+    void handle();
+
+    void onOtaStart(std::function<void()> f);
+    void onOtaEnd(std::function<void()> f);
+
+    private:
+    std::function<void()> otaStartCallback;
+    std::function<void()> otaEndCallback;
+};
 }
 
 #endif
