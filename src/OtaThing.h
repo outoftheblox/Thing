63455290bc1ad61452decf13fa3409a901198cb8
Gerben <gerbenvaneerten@gmail.com>
2019-12-08 11:46:48 +0100

    Fixed indenting

--- src/OtaThing.h
+++ src/OtaThing.h
@@ -7,31 +7,26 @@
 namespace ootb
 {
 
-class OtaThing
-{
-    public:
-    OtaThing();
-    OtaThing(const char* password);
-    ~OtaThing();
+    class OtaThing
+    {
+        public:
+            OtaThing();
+            OtaThing(const char* password);
+            ~OtaThing();
 
-    void setPassword(String _password);
+            void setPassword(String _password);
 
-    void begin();
-    void handle();
+            void begin();
+            void handle();
 
-    void onOtaStart(std::function<void()> f);
-    void onOtaEnd(std::function<void()> f);
+            void onOtaStart(std::function<void()> f);
+            void onOtaEnd(std::function<void()> f);
 
-    private:
-    std::function<void()> otaStartCallback;
-    std::function<void()> otaEndCallback;
-};
-
-    private:
-    String password;
-    std::function<void()> otaStartCallback;
-    std::function<void()> otaEndCallback;
-};
+        private:
+            String password;
+            std::function<void()> otaStartCallback;
+            std::function<void()> otaEndCallback;
+    };
 
 }
 
